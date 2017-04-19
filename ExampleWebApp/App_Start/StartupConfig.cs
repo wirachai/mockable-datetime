b2d5478d85adfc443c20a695c61a0d415489e6ad
Wirachai <wirachai.me@gmail.com>
2017-04-19 22:45:25 +0700

    Add a mechanic for setting system startup time

---
+++ ExampleWebApp/App_Start/StartupConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExampleWebApp.Properties;
+using MockableDateTime;
+
+namespace ExampleWebApp
+{
+    public class StartupConfig
+    {
+        public static void Initial()
+        {
+            var systemStartupTime = Settings.Default.SystemStartupTime;
+            if (systemStartupTime != DateTime.MinValue)
+            {
+                SystemTime.SetDateTime(systemStartupTime);
+            }
+        }
+    }
+}
--- ExampleWebApp/Global.asax.cs
+++ ExampleWebApp/Global.asax.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ExampleWebApp
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            StartupConfig.Initial();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
